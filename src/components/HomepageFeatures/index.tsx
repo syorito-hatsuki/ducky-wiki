276f051ee12e05e305544a395a6b20f17eb5a881
syorito-hatsuki <enderdron3@gmail.com>
2025-04-03 01:52:26 +0300

    Updated intro screen

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,71 +1,109 @@
 import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
+import NoIcon from '@site/static/img/no-icon.svg';
+import Link from "@docusaurus/Link";
+
 import styles from './styles.module.css';
 
 type FeatureItem = {
     title: string;
-    Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+    img?: string;
     description: ReactNode;
+    docs: string;
 };
 
-const FeatureList: FeatureItem[] = [
+const ModsList: FeatureItem[] = [
     {
-        title: 'Easy to Use',
-        Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-        description: (
-            <>
-                Docusaurus was designed from the ground up to be easily installed and
-                used to get your website up and running quickly.
-            </>
-        ),
+        title: 'Death Counter',
+        img: "https://github.com/syorito-hatsuki/death-counter/blob/1.21.2/src/main/resources/assets/deathcounter/icon.png?raw=true",
+        description: (<>Death Counter with client Web UI</>),
+        docs: "death-counter"
     },
     {
-        title: 'Focus on What Matters',
-        Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-        description: (
-            <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-                ahead and move your docs into the <code>docs</code> directory.
-            </>
-        ),
+        title: 'Ducky Updater: ReWork',
+        img: "https://github.com/syorito-hatsuki/ducky-updater-rework/blob/1.20/src/main/resources/assets/duckyupdaterrework/icon.png?raw=true",
+        description: (<>Simple utility for update mods using Modrinth API</>),
+        docs: "ducky-updater-rework"
     },
     {
-        title: 'Powered by React',
-        Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-        description: (
-            <>
-                Extend or customize your website layout by reusing React. Docusaurus can
-                be extended while reusing the same header and footer.
-            </>
-        ),
+        title: 'Yet Another Cobblestone Generator',
+        img: "https://github.com/syorito-hatsuki/yet-another-cobble-gen/blob/1.21/src/main/resources/assets/yacg/icon.png?raw=true",
+        description: (<>Flexible customizable generators</>),
+        docs: "yet-another-cobblestone-generator"
     },
 ];
+const LibrariesList: FeatureItem[] = [
+    {
+        title: "Ducky Updater Lib",
+        img: "https://github.com/syorito-hatsuki/ducky-updater-lib/blob/1.20/src/main/resources/assets/duckyupdater/icon.png?raw=true",
+        description: "Simple library for checking mod updates from modrinth",
+        docs: "ducky-updater-lib"
+    },
+    {
+        title: "ModMenu Badges Lib",
+        description: "More badge!!!",
+        docs: "modmenu-badges-lib"
+    },
+]
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, img, description, docs}: FeatureItem) {
     return (
-        <div className={clsx('col col--4')}>
-            <div className="text--center">
-                <Svg className={styles.featureSvg} role="img"/>
-            </div>
-            <div className="text--center padding-horiz--md">
-                <Heading as="h3">{title}</Heading>
-                <p>{description}</p>
-            </div>
+        <div className={clsx('col col--3')}>
+            <Link to={`/docs/${docs}`} className={styles.featureLink}>
+                <div className="text--center">
+                    {img ? <img alt={title} src={img}
+                                style={{imageRendering: 'pixelated', height: "200px", width: "200px"}}/> :
+                        <NoIcon style={{height: "200px", width: "200px"}}/>}
+                </div>
+                <div className="text--center padding-horiz--md">
+                    <Heading as="h3">{title}</Heading>
+                    <p>{description}</p>
+                </div>
+            </Link>
         </div>
     );
 }
 
 export default function HomepageFeatures(): ReactNode {
-    return (
-        <section className={styles.features}>
-            <div className="container">
-                <div className="row">
-                    {FeatureList.map((props, idx) => (
-                        <Feature key={idx} {...props} />
-                    ))}
+    return (<section style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "2rem 0",
+            width: "100%"
+        }}>
+            <h1>Mods</h1>
+            <section style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "2rem 0",
+                width: "100%"
+            }}>
+                <div className="container">
+                    <div className="row">
+                        {ModsList.map((props, idx) => (
+                            <Feature key={idx} {...props} />
+                        ))}
+                    </div>
                 </div>
-            </div>
+            </section>
+
+            <h1>Libraries</h1>
+            <section style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "2rem 0",
+                width: "100%"
+            }}>
+                <div className="container">
+                    <div className="row">
+                        {LibrariesList.map((props, idx) => (
+                            <Feature key={idx} {...props} />
+                        ))}
+                    </div>
+                </div>
+            </section>
         </section>
     );
 }
